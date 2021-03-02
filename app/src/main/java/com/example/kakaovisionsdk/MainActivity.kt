294433bf0144e113f9aaed66acfbcca9212623c8
mbkim95 <mbkim95@naver.com>
2021-03-02 21:27:05 +0900

    샘플 앱 코드 일부 구현

--- app/src/main/java/com/example/kakaovisionsdk/MainActivity.kt
+++ app/src/main/java/com/example/kakaovisionsdk/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.kakaovisionsdk
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kakaovisionsdk.databinding.ActivityMainBinding
+import com.example.vision.VisionApiClient
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            selectBtn.setOnClickListener {
+                VisionApiClient.instance.selectImage(applicationContext) {
+
+                }
+            }
+        }
     }
+
+
 }
