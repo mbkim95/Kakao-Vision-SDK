f26725a5c3fbc90652beaa0aa9d4951d261ebfcc
mbkim95 <mbkim95@naver.com>
2021-03-04 11:59:23 +0900

    OCR API 결과 callback으로 리턴하도록 구현

--- vision/src/main/java/com/example/vision/VisionApiClient.kt
+++ vision/src/main/java/com/example/vision/VisionApiClient.kt
@@ -7,30 +7,43 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.ResultReceiver
-import android.util.Log
 import com.example.vision.ImageSelectActivity.Companion.OCR_RESULT
 import com.example.vision.model.OcrResult
 
 class VisionApiClient {
+    fun getOcrResult(context: Context, lineBreak: Boolean, callback: (String) -> Unit) {
+        selectImage(context, lineBreak, callback)
+    }
 
-    fun selectImage(context: Context, callback: () -> Unit) {
+    private fun selectImage(context: Context, lineBreak: Boolean, callback: (String) -> Unit) {
         context.startActivity(Intent(context, ImageSelectActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
             putExtra(IntentFactory.KEY_BUNDLE, Bundle().apply {
-                putParcelable(IntentFactory.IMAGE_SELECTOR, resultReceiver(callback))
+                putParcelable(IntentFactory.IMAGE_SELECTOR, resultReceiver(lineBreak, callback))
             })
         })
     }
 
     @JvmSynthetic
-    internal fun resultReceiver(callback: () -> Unit): ResultReceiver {
+    internal fun resultReceiver(lineBreak: Boolean, callback: (String) -> Unit): ResultReceiver {
         return object : ResultReceiver(Handler(Looper.getMainLooper())) {
             override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
                 if (resultCode == Activity.RESULT_OK) {
-                    Log.d(
-                        "Kakao Vision",
-                        "onReceiveResult: ${resultData?.getParcelable<OcrResult>(OCR_RESULT)}"
-                    )
+                    resultData?.let {
+                        val result = it.getParcelable<OcrResult>(OCR_RESULT)
+                        val sb = StringBuilder()
+                        result?.ocrDetailResult?.forEach { ocr ->
+                            sb.append(ocr.recognitionWords[0] + " ")
+                        }
+                        val sentences = sb.toString().run {
+                            if (lineBreak) {
+                                this.replace(".", ".\n").replace("?", "?\n").replace("!", "!\n")
+                            } else {
+                                this
+                            }
+                        }
+                        callback(sentences)
+                    }
                     // api 호출 결과 처리
                 } else if (resultCode == Activity.RESULT_CANCELED) {
                     /*
